0685bd94db2eadba64fbfe9bf461f2f9cbafe6c1
DenisOffor <denlobanov5@mail.ru>
2024-03-25 08:25:11 +0300

    Mux32in1 added for choose data transmitted is the moment. Top module modifed

--- mux32in1.v
+++ mux32in1.v
@@ -1,16 +1,139 @@
 module mux32in1 #(parameter DATA_LENGTH = 8)
 (
-	input 	[DATA_LENGTH-1:0]		in1,
-	input 	[DATA_LENGTH-1:0]		in2,
-	input 	[DATA_LENGTH-1:0]		in3,
-	input 	[DATA_LENGTH-1:0]		in4,
-	input 	[DATA_LENGTH-1:0]		in5,
-	input 	[DATA_LENGTH-1:0]		in6,
-	input 	[DATA_LENGTH-1:0]		in7,
-	input 	[DATA_LENGTH-1:0]		in8,
-	input 	[DATA_LENGTH-1:0]		in9,
-	input 	[DATA_LENGTH-1:0]		in10
+	input 			[DATA_LENGTH-1:0]		in0,
+	input 			[DATA_LENGTH-1:0]		in1,
+	input 			[DATA_LENGTH-1:0]		in2,
+	input 			[DATA_LENGTH-1:0]		in3,
+	input 			[DATA_LENGTH-1:0]		in4,
+	input 			[DATA_LENGTH-1:0]		in5,
+	input 			[DATA_LENGTH-1:0]		in6,
+	input 			[DATA_LENGTH-1:0]		in7,
+	input 			[DATA_LENGTH-1:0]		in8,
+	input 			[DATA_LENGTH-1:0]		in9,
+	input 			[DATA_LENGTH-1:0]		in10,
+	input 			[DATA_LENGTH-1:0]		in11,
+	input 			[DATA_LENGTH-1:0]		in12,
+	input 			[DATA_LENGTH-1:0]		in13,
+	input 			[DATA_LENGTH-1:0]		in14,
+	input 			[DATA_LENGTH-1:0]		in15,
+	input 			[DATA_LENGTH-1:0]		in16,
+	input 			[DATA_LENGTH-1:0]		in17,
+	input 			[DATA_LENGTH-1:0]		in18,
+	input 			[DATA_LENGTH-1:0]		in19,
+	input 			[DATA_LENGTH-1:0]		in20,
+	input 			[DATA_LENGTH-1:0]		in21,
+	input 			[DATA_LENGTH-1:0]		in22,
+	input 			[DATA_LENGTH-1:0]		in23,
+	input 			[DATA_LENGTH-1:0]		in24,
+	input 			[DATA_LENGTH-1:0]		in25,
+	input 			[DATA_LENGTH-1:0]		in26,
+	input 			[DATA_LENGTH-1:0]		in27,
+	input 			[DATA_LENGTH-1:0]		in28,
+	input 			[DATA_LENGTH-1:0]		in29,
+	input 			[DATA_LENGTH-1:0]		in30,
+	input 			[DATA_LENGTH-1:0]		in31,
+	
+	input   		[5:0]					sel,
+	output	reg		[DATA_LENGTH-1:0]		out
 );
-
-
+	always @(*) begin
+		case(sel)
+			0: begin
+				out <= in0;
+			end
+			1: begin
+				out <= in1;
+			end
+			2: begin
+				out <= in2;
+			end
+			3: begin
+				out <= in3;
+			end
+			4: begin
+				out <= in4;
+			end
+			5: begin
+				out <= in5;
+			end
+			6: begin
+				out <= in6;
+			end
+			7: begin
+				out <= in7;
+			end
+			8: begin
+				out <= in8;
+			end
+			9: begin
+				out <= in9;
+			end
+			10: begin
+				out <= in10;
+			end
+			11: begin
+				out <= in11;
+			end
+			12: begin
+				out <= in12;
+			end
+			13: begin
+				out <= in13;
+			end
+			14: begin
+				out <= in14;
+			end
+			15: begin
+				out <= in15;
+			end
+			16: begin
+				out <= in16;
+			end
+			17: begin
+				out <= in17;
+			end
+			18: begin
+				out <= in18;
+			end
+			19: begin
+				out <= in19;
+			end
+			20: begin
+				out <= in20;
+			end
+			21: begin
+				out <= in21;
+			end
+			22: begin
+				out <= in22;
+			end
+			23: begin
+				out <= in23;
+			end
+			24: begin
+				out <= in24;
+			end
+			25: begin
+				out <= in25;
+			end
+			26: begin
+				out <= in26;
+			end
+			27: begin
+				out <= in27;
+			end
+			28: begin
+				out <= in28;
+			end
+			29: begin
+				out <= in29;
+			end
+			30: begin
+				out <= in30;
+			end
+			31: begin
+				out <= in31;
+			end
+		endcase
+	end
 endmodule
--- top.v
+++ top.v
@@ -12,7 +12,7 @@ module top #(parameter FFT_SIZE = 16, WORD_SIZE = 16, DATA_LENGTH = 8, FRACTION 
 	wire 							w_RX_error;
 
 	//reg and wires for UART_TX
-	reg 	[DATA_LENGTH - 1:0] 	r_Transmitted_byte;
+	wire 	[DATA_LENGTH - 1:0] 	w_Transmitted_byte;
 	reg								r_TX_start;
 	reg								r_full_TX_state; //1 - process, 0 - wait
 	wire 							w_TX_start;
@@ -26,37 +26,21 @@ module top #(parameter FFT_SIZE = 16, WORD_SIZE = 16, DATA_LENGTH = 8, FRACTION 
 	
 	//wires for out from FFT
 	wire	[WORD_SIZE-1:0]			w_FFT_out0_re;
-	wire	[WORD_SIZE-1:0]			w_FFT_out0_im;
 	wire	[WORD_SIZE-1:0]			w_FFT_out1_re;
-	wire	[WORD_SIZE-1:0]			w_FFT_out1_im;
 	wire	[WORD_SIZE-1:0]			w_FFT_out2_re;
-	wire	[WORD_SIZE-1:0]			w_FFT_out2_im;
 	wire	[WORD_SIZE-1:0]			w_FFT_out3_re;
-	wire	[WORD_SIZE-1:0]			w_FFT_out3_im;
 	wire	[WORD_SIZE-1:0]			w_FFT_out4_re;
-	wire	[WORD_SIZE-1:0]			w_FFT_out4_im;
 	wire	[WORD_SIZE-1:0]			w_FFT_out5_re;
-	wire	[WORD_SIZE-1:0]			w_FFT_out5_im;
 	wire	[WORD_SIZE-1:0]			w_FFT_out6_re;
-	wire	[WORD_SIZE-1:0]			w_FFT_out6_im;
 	wire	[WORD_SIZE-1:0]			w_FFT_out7_re;
-	wire	[WORD_SIZE-1:0]			w_FFT_out7_im;
 	wire	[WORD_SIZE-1:0]			w_FFT_out8_re;
-	wire	[WORD_SIZE-1:0]			w_FFT_out8_im;
 	wire	[WORD_SIZE-1:0]			w_FFT_out9_re;
-	wire	[WORD_SIZE-1:0]			w_FFT_out9_im;
 	wire	[WORD_SIZE-1:0]			w_FFT_out10_re;
-	wire	[WORD_SIZE-1:0]			w_FFT_out10_im;
 	wire	[WORD_SIZE-1:0]			w_FFT_out11_re;
-    wire	[WORD_SIZE-1:0]			w_FFT_out11_im;
     wire	[WORD_SIZE-1:0]			w_FFT_out12_re;
-    wire	[WORD_SIZE-1:0]			w_FFT_out12_im;
     wire	[WORD_SIZE-1:0]			w_FFT_out13_re;
-    wire	[WORD_SIZE-1:0]			w_FFT_out13_im;
     wire	[WORD_SIZE-1:0]			w_FFT_out14_re;
-    wire	[WORD_SIZE-1:0]			w_FFT_out14_im;
     wire	[WORD_SIZE-1:0]			w_FFT_out15_re;
-    wire	[WORD_SIZE-1:0]			w_FFT_out15_im;
 	wire							w_FFT_cycle_done;
 	
 	//wire and reg for reset FFT
@@ -115,6 +99,46 @@ module top #(parameter FFT_SIZE = 16, WORD_SIZE = 16, DATA_LENGTH = 8, FRACTION 
 		end
 	end
 	
+	mux32in1 #(.DATA_LENGTH(DATA_LENGTH))
+	(
+		.in0(w_FFT_out0_re[7:0]),
+		.in1(w_FFT_out0_re[15:8]),
+		.in2(w_FFT_out1_re[7:0]),
+		.in3(w_FFT_out1_re[15:8]),
+		.in4(w_FFT_out2_re[7:0]),
+		.in5(w_FFT_out2_re[15:8]),
+		.in6(w_FFT_out3_re[7:0]),
+		.in7(w_FFT_out3_re[15:8]),
+		.in8(w_FFT_out4_re[7:0]),
+		.in9(w_FFT_out4_re[15:8]),
+		.in10(w_FFT_out5_re[7:0]),
+		.in11(w_FFT_out5_re[15:8]),
+		.in12(w_FFT_out6_re[7:0]),
+		.in13(w_FFT_out6_re[15:8]),
+		.in14(w_FFT_out7_re[7:0]),
+		.in15(w_FFT_out7_re[15:8]),
+		.in16(w_FFT_out8_re[7:0]),
+		.in17(w_FFT_out8_re[15:8]),
+		.in18(w_FFT_out9_re[7:0]),
+		.in19(w_FFT_out9_re[15:8]),
+		.in20(w_FFT_out10_re[7:0]),
+		.in21(w_FFT_out10_re[15:8]),
+		.in22(w_FFT_out11_re[7:0]),
+		.in23(w_FFT_out11_re[15:8]),
+		.in24(w_FFT_out12_re[7:0]),
+		.in25(w_FFT_out12_re[15:8]),
+		.in26(w_FFT_out13_re[7:0]),
+		.in27(w_FFT_out13_re[15:8]),
+		.in28(w_FFT_out14_re[7:0]),
+		.in29(w_FFT_out14_re[15:8]),
+		.in30(w_FFT_out15_re[7:0]),
+		.in31(w_FFT_out15_re[15:8]),
+		
+		.sel(counter_of_sended_bytes),
+		.out(w_Transmitted_byte)
+	);
+	
+	
 	FFT_for_OFDM #(.WORD_SIZE(WORD_SIZE),.DATA_LENGTH(DATA_LENGTH), .FRACTION(FRACTION), .STAGES(STAGES)) FFT_for_OFDM
 	(
 		.i_clk(i_clk),
@@ -122,37 +146,37 @@ module top #(parameter FFT_SIZE = 16, WORD_SIZE = 16, DATA_LENGTH = 8, FRACTION 
 		.i_byte(w_Received_byte),
 	
 		.out0_re(w_FFT_out0_re),
-		.out0_im(w_FFT_out0_im),
+		.out0_im(),
 		.out1_re(w_FFT_out1_re),
-		.out1_im(w_FFT_out1_im),
+		.out1_im(),
 		.out2_re(w_FFT_out2_re),
-		.out2_im(w_FFT_out2_im),
+		.out2_im(),
 		.out3_re(w_FFT_out3_re),
-		.out3_im(w_FFT_out3_im),
+		.out3_im(),
 		.out4_re(w_FFT_out4_re),
-		.out4_im(w_FFT_out4_im),
+		.out4_im(),
 		.out5_re(w_FFT_out5_re),
-		.out5_im(w_FFT_out5_im),
+		.out5_im(),
 		.out6_re(w_FFT_out6_re),
-		.out6_im(w_FFT_out6_im),
+		.out6_im(),
 		.out7_re(w_FFT_out7_re),
-		.out7_im(w_FFT_out7_im),
+		.out7_im(),
 		.out8_re(w_FFT_out8_re),
-		.out8_im(w_FFT_out8_im),
+		.out8_im(),
 		.out9_re(w_FFT_out9_re),
-		.out9_im(w_FFT_out9_im),
+		.out9_im(),
 		.out10_re(w_FFT_out10_re),
-		.out10_im(w_FFT_out10_im),
+		.out10_im(),
 		.out11_re(w_FFT_out11_re),
-		.out11_im(w_FFT_out11_im),
+		.out11_im(),
 		.out12_re(w_FFT_out12_re),
-		.out12_im(w_FFT_out12_im),
+		.out12_im(),
 		.out13_re(w_FFT_out13_re),
-		.out13_im(w_FFT_out13_im),
+		.out13_im(),
 		.out14_re(w_FFT_out14_re),
-		.out14_im(w_FFT_out14_im),
+		.out14_im(),
 		.out15_re(w_FFT_out15_re),
-		.out15_im(w_FFT_out15_im),
+		.out15_im(),
 		
 		.o_FFT_cycle_done(w_FFT_cycle_done)
 	);
@@ -171,7 +195,7 @@ module top #(parameter FFT_SIZE = 16, WORD_SIZE = 16, DATA_LENGTH = 8, FRACTION 
 		.i_clk(i_clk),
 		.i_rst(~r_full_TX_state),
 		.i_start(w_TX_start), 
-		.i_TX_byte(r_Transmitted_byte),
+		.i_TX_byte(w_Transmitted_byte),
 		.o_TX_bit(o_TX_bit), 
 		.o_transfer_state(w_transfer_state), 
 		.o_TX_done(w_TX_done)
